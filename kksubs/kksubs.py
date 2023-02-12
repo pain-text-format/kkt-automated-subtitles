286e4888180656229f808430c580bf1401f282c1
roach <113860476+labroach22@users.noreply.github.com>
2023-02-12 00:54:00 -0800

    add yaml config support

--- kksubs/kksubs.py
+++ kksubs/kksubs.py
@@ -1,10 +1,25 @@
 import json
 import logging
+import os
+
+import yaml
 
 from kksubs.model.data_access_services import SubtitleDataAccessService
 from kksubs.model.subtitle_services import SubtitleService
 
 logger = logging.getLogger(__name__)
+
+def _get_config_file_dict(filepath):
+    extension = os.path.splitext(filepath)[1]
+    if extension in {".json"}:
+        with open(filepath, "r", encoding="utf-8") as json_reader:
+            content = json.load(json_reader)
+    elif extension in {".yml", ".yaml"}:
+        with open(filepath, "r", encoding="utf-8") as yaml_reader:
+            content = yaml.safe_load(yaml_reader)
+    else:
+        raise TypeError(f"Invalid file type for config: {filepath}")
+    return content
 
 class SubtitleController:
 
@@ -38,23 +53,22 @@ class SubtitleController:
         self.subtitle_model.set_default_subtitle_profile_id(default_subtitle_profile_id)
 
     def load_configs(self, filepath):
-        with open(filepath, "r") as json_file:
-            configs = json.load(json_file)
+        configs_dict = _get_config_file_dict(filepath)
 
-        input_text_directory = configs["input_text_directory"]
-        input_image_directory = configs["input_image_directory"]
-        output_directory = configs["output_directory"]
+        input_text_directory = configs_dict["input_text_directory"]
+        input_image_directory = configs_dict["input_image_directory"]
+        output_directory = configs_dict["output_directory"]
 
         self.load_input_text_directory(input_text_directory)
         self.load_input_image_directory(input_image_directory)
         self.load_output_directory(output_directory)
 
-        if "subtitle_profile_path" in configs.keys():
-            subtitle_profile_path = configs["subtitle_profile_path"]  # may not exist
+        if "subtitle_profile_path" in configs_dict.keys():
+            subtitle_profile_path = configs_dict["subtitle_profile_path"]  # may not exist
             self.load_subtitle_profiles(subtitle_profile_path)
 
-            if "default_subtitle_profile_id" in configs.keys():
-                self.load_default_subtitle_profile_id(configs["default_subtitle_profile_id"])
+            if "default_subtitle_profile_id" in configs_dict.keys():
+                self.load_default_subtitle_profile_id(configs_dict["default_subtitle_profile_id"])
 
     def create_yaml_template(self, filename, existing_filename:str=None):
         # Creates an appropriately formatted text file in the input text directory for the user to work on.
